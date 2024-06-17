f490ffd0e9c03e8004f20a52e7e710d0788bfc5e
Emerson Arruda <emersonarruda753@gmail.com>
2024-06-17 19:50:18 -0300

    Aula 06 - Utilizar a classe ResponseEntity, do Spring, para personalizar os retornos dos métodos de uma classe Controller, Modificar o código HTTP devolvido nas respostas da API, Adicionar cabeçalhos nas respostas da API, e Utilizar os códigos HTTP mais apropriados para cada operação realizada na API.

---
+++ src/main/java/br/com/alura/medvoll/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,10 @@
+package br.com.alura.medvoll.paciente;
+
+import br.com.alura.medvoll.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+    }
+}
